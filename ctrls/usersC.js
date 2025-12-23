0f5bc127332e44845f2d29e06e67f4c05870c645
ShayShN <shayshn1234@gmail.com>
2025-12-23 18:00:47 +0200

    finish day

--- ctrls/usersC.js
+++ ctrls/usersC.js
@@ -21,5 +21,18 @@ export const getAllUsers = async (req, res) => {
 
 
 export const updateUser = async (req, res) => {
+  try {
+      const data = await readFile(pathUsers)
+      const newPassword = req.body.password
+      const foundName = data.findIndex(user => user.username === req.params.username)
+      if (foundName === -1) {
+          return res.status(403).json("not found name")
+      }
+      data[foundName].password = newPassword
+      await writeFile(pathUsers, data)
+      res.json(data[foundName])
+  } catch (err) {
+    console.error(err);
     
+  }
 }
